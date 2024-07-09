97ecc96eeead91ea45e5146e45afd246a8538342
LeiChen9 <chenlei9691@gmail.com>
2024-07-09 23:29:21 +0800

    add data split

--- Transformer/run.py
+++ Transformer/run.py
@@ -2,13 +2,14 @@
 Author: LeiChen9 chenlei9691@gmail.com
 Date: 2024-07-09 23:10:27
 LastEditors: LeiChen9 chenlei9691@gmail.com
-LastEditTime: 2024-07-09 23:23:41
+LastEditTime: 2024-07-09 23:28:44
 FilePath: /SpeechDepDiag/Users/lei/Documents/Code/Vanilla/Transformer/run.py
 Description: 
 
 Copyright (c) 2024 by Riceball, All Rights Reserved. 
 '''
 import torch
+import pdb 
 with open("WestWorld.txt", 'r', encoding='utf-8') as f:
     text = f.read()
 
@@ -31,3 +32,14 @@ print(decode(encode("hello world")))
 '''
 
 data = torch.tensor(encode(text), dtype=torch.long )
+
+data_len = data.size()[0]
+train_len = int(data_len * 0.8)
+
+train_data, val_data = data[:train_len], data[train_len:]
+
+batch_size = 4
+block_size = 8
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
