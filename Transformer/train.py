7aa8fd9507685a59992b93dd5cb21a04bcc67c0f
LeiChen9 <chenlei9691@gmail.com>
2024-07-16 08:05:12 +0000

    done with encoding

--- Transformer/train.py
+++ Transformer/train.py
@@ -8,10 +8,12 @@ print("Unique word in text: {}".format(len(set(text))))
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
 
+# encoding chars, can be replaced by SentencePiece of Google
+# or titoken by OpenAI
 stoi = {ch: i for i, ch in enumerate(chars)}
 itos = {i: ch for i, ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
 
 print(encode('hello, world'))
-print(decode(encode('hello, world')))
+print(decode(encode('hello, world'))) 
