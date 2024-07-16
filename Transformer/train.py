541509a19d3af6ff335154d2d1b92158279f5a2a
LeiChen9 <chenlei9691@gmail.com>
2024-07-16 08:01:01 +0000

    xxx

---
+++ Transformer/train.py
@@ -0,0 +1,17 @@
+# open file
+with open("WestWorld.txt", "r", encoding='utf-8') as f:
+    text = f.read()
+
+print("len of text: {}".format(len(text)))
+print("Unique word in text: {}".format(len(set(text))))
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+print(encode('hello, world'))
+print(decode(encode('hello, world')))
