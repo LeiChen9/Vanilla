aa950ba586e5bf9e3bd0c176d6cac8ae79156a1d
LeiChen9 <chenlei9691@gmail.com>
2024-07-16 08:49:43 +0000

    add gen

--- Transformer/train.py
+++ Transformer/train.py
@@ -54,6 +54,15 @@ class BigramLM(nn.Module):
         loss = F.cross_entropy(logits, targets)
 
         return logits, loss
+    
+    def gen(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
 
 m = BigramLM(vocab_size)
 out, loss = m(xb, yb)
