6d6ac6f043a2873c04faed6b136ea8dadec2da89
LeiChen9 <chenlei9691@gmail.com>
2024-07-16 09:20:36 +0000

    add eval

--- Transformer/train.py
+++ Transformer/train.py
@@ -2,6 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# define hyper params
+batch_size = 32
+block_size = 8
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else "cpu"
+eval_iters = 200
+
+torch.manual_seed(3)
+
 # open file
 with open("WestWorld.txt", "r", encoding='utf-8') as f:
     text = f.read()
@@ -35,7 +46,7 @@ def get_batch(split):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    return x, y
+    return x.to(device), y.to(device)
 
 xb, yb = get_batch('train')
 
@@ -66,23 +77,39 @@ class BigramLM(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
 
-m = BigramLM(vocab_size)
+m = BigramLM(vocab_size).to(device)
 out, loss = m(xb, yb)
 print(out.shape)
 print(loss)
 
 optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
 
-batch_size = 32
-for steps in range(10000):
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            x, y = get_batch('split')
+            logits, loss = model(x, y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+for iter in range(max_iters):
+
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"Step {iter}: train loss: {losses['train']:.4f}, val loss: {losses['val']:.4f}")
+    
     xb, yb = get_batch('train')
 
-    logits, loss = m(xb, yb)
+    logits, loss = model(xb, yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
 
-print(loss.item())
-
-idx = torch.zeros((1, 1), dtype=torch.long)
+idx = torch.zeros((1, 1), dtype=torch.long, device=devcie)
 print(decode(m.gen(idx, 100)[0].tolist()))
